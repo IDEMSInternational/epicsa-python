b1e9f831a01e76f3720e91b6d06108de5dc354e4
Stephen Lloyd <stephen.lloyd03@gmail.com>
2023-07-07 10:39:26 +0200

    Add version number to epicsa-python

---
+++ epicsa_python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.0.3',
+    description='A description of my package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://example.com/my_package',
+    packages=find_packages(),
+)
